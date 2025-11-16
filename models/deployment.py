4567c4b1baa91ae155df7aa4bc46197ce20d0030
dcsand <90474498+dcsand@users.noreply.github.com>
2025-11-16 12:09:18 -0500

    file to run in console

---
+++ models/deployment.py
@@ -0,0 +1,42 @@
+import pickle
+import pandas as pd
+import numpy as np
+import pickle
+from scipy.stats import randint, uniform
+
+# Preprocesamiento y Pipeline
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.preprocessing import LabelEncoder, RobustScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+# ¡Importaciones clave para el transformador personalizado!
+from sklearn.base import BaseEstimator, TransformerMixin
+
+# Modelo
+from xgboost import XGBClassifier
+
+import pickle
+
+SRC_PATH = '/content/drive/MyDrive/Pregrado - Posgrado - Trabajo/Maestría - Inteligencia Artificial Aplicada/11. MLOps/1. Primera etapa de proyecto/Modelado/obesity_estimation_original.csv'
+MODEL_PATH = 'xgboost_model_artifacts.pkl'
+
+with open(MODEL_PATH, 'rb') as f:
+    artifacts = pickle.load(f)
+
+model = artifacts['model']
+label_encoder = artifacts['label_encoder']
+
+df_raw = pd.read_csv(SRC_PATH, sep=None, engine="python", encoding="utf-8")
+
+# new_data_raw es un DataFrame con los datos crudos
+new_data_cleaned = df_raw # (Aquí aplicas la lógica de DataManager)
+
+# 1. Predecir las clases numéricas
+pred_numeric = model.predict(new_data_cleaned)
+
+# 2. Predecir las probabilidades (si las necesitas)
+pred_proba = model.predict_proba(new_data_cleaned)
+
+# 3. Decodificar a etiquetas legibles
+pred_labels = label_encoder.inverse_transform(pred_numeric)
