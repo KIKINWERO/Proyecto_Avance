33820c8dfd895cc2500f7c0b92851e7dda516f29
dcsand <90474498+dcsand@users.noreply.github.com>
2025-11-16 12:12:02 -0500

    remove redundant definition

--- models/deployment.py
+++ models/deployment.py
@@ -29,14 +29,12 @@ label_encoder = artifacts['label_encoder']
 
 df_raw = pd.read_csv(SRC_PATH, sep=None, engine="python", encoding="utf-8")
 
-# new_data_raw es un DataFrame con los datos crudos
-new_data_cleaned = df_raw # (Aquí aplicas la lógica de DataManager)
-
 # 1. Predecir las clases numéricas
-pred_numeric = model.predict(new_data_cleaned)
+pred_numeric = model.predict(df_raw)
 
 # 2. Predecir las probabilidades (si las necesitas)
-pred_proba = model.predict_proba(new_data_cleaned)
+pred_proba = model.predict_proba(df_raw)
 
 # 3. Decodificar a etiquetas legibles
+
 pred_labels = label_encoder.inverse_transform(pred_numeric)
